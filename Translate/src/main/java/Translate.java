9d49f14049e1b96da51344c333fd9d8d32a9637a
NITISH SHARMA <nisharma@redhat.com>
2020-09-21 14:15:47 +0530

    first commit

--- Translate/src/main/java/Translate.java
+++ Translate/src/main/java/Translate.java
@@ -5,21 +5,17 @@ import com.google.gson.*;
 import com.squareup.okhttp.*;
 
 public class Translate {
-    /*  Configure the local environment:
-    * Set the TRANSLATOR_TEXT_SUBSCRIPTION_KEY and TRANSLATOR_TEXT_ENDPOINT environment
-    * variables on your local machine using the appropriate method for your
-    * preferred shell (Bash, PowerShell, Command Prompt, etc.). 
-    *
-    * For TRANSLATOR_TEXT_ENDPOINT, use the same region you used to get your
-    * subscription keys.
-    *
-    * If the environment variable is created after the application is launched
-    * in a console or with Visual Studio, the shell (or Visual Studio) needs to be
-    * closed and reloaded to take the environment variable into account.
-    */
-    private static String subscriptionKey = System.getenv("TRANSLATOR_TEXT_SUBSCRIPTION_KEY");
-    private static String endpoint = System.getenv("TRANSLATOR_TEXT_ENDPOINT");
-    String url = endpoint + "/translate?api-version=3.0&to=de,it";
+    private static String subscriptionKey = "private-key";
+
+    HttpUrl url = new HttpUrl.Builder()
+        .scheme("https")
+        .host("api.cognitive.microsofttranslator.com")
+        .addPathSegment("/translate")
+        .addQueryParameter("api-version", "3.0")
+        .addQueryParameter("from", "en")
+        .addQueryParameter("to", "de")
+        .addQueryParameter("to", "it")
+        .build();
 
     // Instantiates the OkHttpClient.
     OkHttpClient client = new OkHttpClient();
@@ -28,11 +24,10 @@ public class Translate {
     public String Post() throws IOException {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType,
-                "[{\n\t\"Text\": \"Welcome to Microsoft Translator. Guess how many languages I speak!\"\n}]");
-        Request request = new Request.Builder()
-                .url(url).post(body)
-                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
-                .addHeader("Content-type", "application/json").build();
+                "[{\"Text\": \"Hello World!\"}]");
+        Request request = new Request.Builder().url(url).post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey).addHeader("Content-type", "application/json")
+                .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
     }
@@ -44,6 +39,7 @@ public class Translate {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(json);
     }
+
     public static void main(String[] args) {
         try {
             Translate translateRequest = new Translate();
